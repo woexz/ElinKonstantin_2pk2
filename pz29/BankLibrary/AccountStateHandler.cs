9a09e07389d048c4ea39359b3814d2033efb9739
woexz <114604765+woexz@users.noreply.github.com>
2023-05-11 12:11:58 +0500

    Add files via upload

---
+++ pz29/BankLibrary/AccountStateHandler.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankLibrary
+{
+    public delegate void AccountStateHandler(object sender, AccountEventArgs e);
+    public class AccountEventArgs
+    {
+        public string Message
+        {
+            get { return Message; }
+            set { Message = value; }
+        }   //автосвойство сообщения (Message)
+        public decimal Sum
+        {
+            get { return Sum; }
+            private set { Sum = value; }
+        }   //автосвойство суммы (Sum), на которую изменился счет
+        public AccountEventArgs(string message, decimal sum) 
+        {
+            Message = message;
+            Sum = sum;
+        }
+        // определите конструктор класса, инициирующий Message и Sum
+    }
+}
---
+++ pz29/BankLibrary/IAccount.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankLibrary
+{
+    internal interface IAccount
+    {
+        public void Put(decimal sum); //метод пополнения счета
+        public decimal Withdraw(decimal sum); //метод снятия со счета
+    }
+}
