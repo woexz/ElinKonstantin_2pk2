9a2ab43375c7c72dcff59bc9894bdd63e5d7bb7a
woexz <114604765+woexz@users.noreply.github.com>
2023-05-14 18:39:32 +0500

    Add files via upload

--- pz26/MainWindow.xaml.cs
+++ pz26/MainWindow.xaml.cs
@@ -12,17 +12,62 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
+using System.Security.AccessControl;
 
-namespace redaktor_txt_pz26
+namespace Wpfpz26
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        public bool saveStatusBool;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Window1 wn = new Window1();
+            wn.ShowDialog();
+            MainTextBox.Text = Window1.mainTBString;//заполнение TextBox текстом из файла.
+        }//открытие диалогового окна
+
+        private void SaveText_Click(object sender, RoutedEventArgs e)
+        {
+            if(Window1.nameFile != null)
+            {
+                FileStream fileStream = new FileStream(Window1.nameFile, FileMode.Open, FileAccess.Write);
+                using (StreamWriter streamWriter = new StreamWriter(fileStream, Encoding.Unicode))
+                {
+                    streamWriter.Write(MainTextBox.Text);
+                }
+                fileStream.Close();
+                SaveStatus.Text = "Сохранено";
+                DateStatus.Text = Convert.ToString(DateTime.Now);
+            }
+        }
+
+        private void StatusSaveVoid(object sender, KeyEventArgs e)
+        {
+            FileStream fileStream = new FileStream(Window1.nameFile, FileMode.Open, FileAccess.Read);
+            using (StreamReader streamReader = new StreamReader(fileStream, Encoding.Unicode))
+            {
+                string fileText = streamReader.ReadToEnd();
+                string textBoxText = MainTextBox.Text;
+                if(fileText != textBoxText)
+                {
+                    SaveStatus.Text = "Требуется сохранение";
+                }
+                else if(fileText == textBoxText)
+                {
+                    SaveStatus.Text = "Сохранено";
+                }
+            }
+            fileStream.Close();
         }
     }
 }
---
+++ pz26/WindowCreateFile.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace Wpfpz26
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public static string nameFile;
+        public static string mainTBString;
+
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void CreateFileOk_Click(object sender, RoutedEventArgs e)
+        {
+            nameFile = $"C:/Users/Артем/Desktop/{NameFile.Text}.txt";
+            if (File.Exists(nameFile))
+            {
+                FileStream fileStream = new FileStream(nameFile, FileMode.Open, FileAccess.Read);
+                using (StreamReader reader = new StreamReader(fileStream))
+                {
+                    mainTBString = reader.ReadToEnd();
+                }
+                fileStream.Close();
+            }
+            else
+            {
+                File.Create(nameFile);
+            }
+            this.DialogResult = true;
+        }//Создание документа по названию или его открытие при вводе названия, которое уже есть. //Закрыте диалогового окна.
+    }
+}
